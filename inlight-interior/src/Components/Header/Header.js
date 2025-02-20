4390ecb8629e00e3d70aa73444be902e70d5bb79
Eldana Kibru <156713119+eldianak21@users.noreply.github.com>
2025-02-20 14:02:18 +0300

    update on header part

--- inlight-interior/src/Components/Header/Header.js
+++ inlight-interior/src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Import the account icon
 
 const services = [
     { name: 'Residential Design', path: '/services#residential-design' },
@@ -11,9 +12,14 @@ const services = [
 
 const Header = () => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [accountDropdownVisible, setAccountDropdownVisible] = useState(false);
 
     const toggleDropdown = () => {
         setDropdownVisible(!dropdownVisible);
+    };
+
+    const toggleAccountDropdown = () => {
+        setAccountDropdownVisible(!accountDropdownVisible);
     };
 
     return (
@@ -25,8 +31,15 @@ const Header = () => {
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/signup">Sign Up</Link></li>
-                    <li><Link to="/login">Sign In</Link></li>
+                    <li onClick={toggleAccountDropdown} className="account-icon">
+                        <AccountCircleIcon />
+                        {accountDropdownVisible && (
+                            <div className="account-dropdown">
+                                <Link to="/signup">Sign Up</Link>
+                                <Link to="/login">Sign In</Link>
+                            </div>
+                        )}
+                    </li>
                 </ul>
             </nav>
             <div className="search-bar" onClick={toggleDropdown}>
